3fe8ae4bbd4bab29c7a63e229f3d76b32a38a82c
DedeSnrwn13 <sunarwandede13@gmail.com>
2023-11-11 16:46:11 +0700

    feat: frontend template

---
+++ resources/views/frontend/includes/footer.blade.php
@@ -0,0 +1,21 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <ul class="social-icons">
+                    <li><a href="#">Facebook</a></li>
+                    <li><a href="#">Twitter</a></li>
+                    <li><a href="#">Behance</a></li>
+                    <li><a href="#">Linkedin</a></li>
+                    <li><a href="#">Dribbble</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-12">
+                <div class="copyright-text">
+                    <p>Copyright {{ date('Y') }} @BLOG D. | Design & Develop By: <a rel="nofollow" href="https://github.com/DedeSnrwn13" target="_parent">Dede Sunarwan</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
